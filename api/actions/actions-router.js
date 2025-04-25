f1f484f1e7ed05a3fcf9e0964aa73db1a9072c35
Jay Marmol <marmoljay@hotmail.com>
2025-04-25 11:58:19 -0400

    finished

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,127 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model')
+const router = express.Router();
+
+
+//GET
+
+router.get('/', async (req, res) => {
+  try {
+   const actions = await Actions.get();
+   if (!actions) {
+    return res.status(404).json({
+        message: 'Action not found'
+    })
+   }
+   res.status(200).json(actions)
+  } catch (err) {
+    res.status(500).json({
+        message:'Failed to get actions'
+    })
+  }
+})
+
+// GET by id
+
+
+router.get('/:id', async (req, res) => {
+    const { id } =req.params
+
+    try{
+         const action = await Actions.get(id)
+
+        if (!action) {
+            return res.status(404).json({
+                message: 'action not found'
+            })
+        }
+       res.status(200).json(action)
+    } catch (err) {
+        res.status(500).json({
+            message: 'Failed to get action'
+        })
+    }
+})
+
+
+
+
+// POST
+
+router.post('/', async (req, res) => {
+    const { project_id, description, notes, completed } = req.body;
+
+    if (!project_id || !description || !notes) {
+        return res.status(400).json({
+            message: 'Missing required project_id, description, or notes'
+        })
+    }
+    try {
+      const newAction = await Actions.insert({project_id,description,notes,completed})
+      res.status(201).json(newAction)
+    } catch (err) {
+        res.status(500).json({
+            message: 'failed to create action'
+        })
+    }
+})
+
+
+
+// PUT
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { project_id,
+         description,
+          notes,
+           completed } =req.body
+
+           if (project_id === undefined ||
+            description === undefined ||
+            notes === undefined||
+            completed === undefined) {
+                return res.status(400).json({
+                    message:'Missing required fields: project_id, description, notes, completed'
+                })
+            }
+            try{
+                const existingAction = await Actions.get(id);
+                if(!existingAction) {
+                    res.status(404).json({
+                        message: 'action not found'
+                    })
+                }
+                const updatedAction = await Actions.update(id, {project_id,
+                    description,notes,completed});
+                    res.json(updatedAction)
+
+            }catch (err) {
+                res.status(500).json({
+                    message:'failed to update action'
+                })
+            }
+})
+
+
+
+//DELETE
+
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+     const deletedAction = await Actions.remove(id)
+     res.status(200).json({
+        message: `Action ${id} deleted`
+     })
+    } catch(err) {
+        res.status(500).json({
+            message: "Failed to delete action"
+        })
+    }
+})
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,145 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const Projects = require('./projects-model')
+
+//GET
+
+router.get('/', async (req, res) => {
+    try{
+     const projects = await Projects.get();
+     res.json(projects)
+    }catch (err) {
+        console.log('error ->', err)
+        res.status(500).json({
+            message:'Failed to retrieve project'
+        });
+    }
+})
+
+// GET by id
+
+router.get('/:id', async(req, res) => {
+    const { id } = req.params;
+
+    try{
+        const project = await Projects.get(id);
+        if(!project) {
+            res.status(404).json({
+                message: 'Project not found'
+            })
+        } else {
+            res.json(project)
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            message:"Failed to retrieve the project"
+        })
+    }
+})
+
+// POST
+
+router.post('/', async (req, res) => {
+    const { name, description, completed } = req.body;
+
+    if(!name || !description) {
+      return  res.status(400).json({
+            message: 'Missing required name or description'
+        })
+    }
+    try {
+        const newProject = await Projects.insert({name, description, completed});
+        res.status(201).json(newProject)
+    } catch (err) {
+        res.status(500).json({
+            message:'Failed to retrieve the project'
+        })
+    }
+})
+
+// PUT
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { name, description, completed } = req.body;
+
+    if (name === undefined ||
+         description === undefined ||
+          completed === undefined) {
+        return res.status(400).json({
+            message: 'Missing requires name or description'
+        })
+    }
+    try {
+      const existingProject = await Projects.get(id);
+      if (!existingProject) {
+        return res.status(404).json({
+            message: 'Project not found'
+        })
+      }
+      const updatedProject = await Projects.update(id, {name, description, completed})
+      res.json(updatedProject)
+    } catch (err) {
+      res.status(500).json({
+        message: 'Failed to retrieve the project'
+      })
+    }
+})
+
+// DELETE
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try{
+        const deleted = await Projects.remove(id);
+
+        if(!deleted) {
+           return res.status(404).json({
+                message: 'Project not found'
+            })
+        }
+        res.status(200).json({
+            message:`Project ${id} deleted`
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message:"Failed to delete the project"
+        })
+    }
+})
+
+// GET actions
+
+router.get('/:id/actions', async (req, res) => {
+    const { id } = req.params
+    try{
+     const project = await Projects.get(id);
+     if (!project) {
+        return res.status(404).json({
+            message: 'Project not found'
+        })
+     }
+     const actions = await Projects.getProjectActions(id);
+     res.json(actions)
+    } catch (err) {
+        res.status(500).json({
+            message:"Failed to get project actions"
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
